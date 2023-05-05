df7b4ebb68f1966350c81d502a100c3f590f1f15
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-05-05 14:26:23 +0300

    option

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import genDiff from "..src/index.js";
+
+ const program = new Command();
+
+ program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+program.parse();
