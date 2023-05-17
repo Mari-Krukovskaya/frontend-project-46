309367b29e852020eb14fba0a6c45c38ebf439c0
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-05-17 15:18:20 +0300

    test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'node:path';
 import fs from 'fs';
@@ -10,22 +10,19 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const fileJson1 = getFixturePath('file1.json');
-const fileJson2 = getFixturePath('file2.json');
-const fileYml1 = getFixturePath('file1.yml');
-const fileYml2 = getFixturePath('file2.yml');
-const expectedStylish = readFile('file.Stylish_result.txt');
-const expectedPlain = readFile('file.FormatPlain_result.txt');
+  const tree = [
+   [ 'file1.json', 'file2.json', 'expectedStylish_result.txt'],
+   [ 'file1.json', 'file2.json', 'expectedStylish_result.txt', 'stylish'],
+   [ 'file1.yml', 'file2.yml', 'expectedStylish_result.txt', 'stylish'],
+   ['file1.json', 'file2.json', 'expectedPlain_result.txt', 'plain'],
+  ];
 
-test('format stylish file json && yml', () => {
-  const actual1 = genDiff(fileJson1, fileJson2);
-  const actual2 = genDiff(fileYml1, fileYml2);
-  expect(actual1).toEqual(expectedStylish);
-  expect(actual2).toEqual(expectedStylish);
-});
-test('format plain file json && yml', () => {
-  const actual3 = genDiff(fileJson1, fileJson2);
-  const actual4 = genDiff(fileYml1, fileYml2);
-  expect(actual3).toEqual(expectedPlain);
-  expect(actual4).toEqual(expectedPlain);
+describe.each(tree)('compare check', (file1, file2, expected, format) => {
+  const data1 = getFixturePath(file1)
+  const data2 = getFixturePath(file2);
+  const actual = genDiff(data1, data2, format);
+  const expetedResult = readFile(expected);
+  test(` test ${file1} && ${file2} with ${format} format to ${expected}`, () => {
+expect(actual).toEqual(expetedResult);
+  });
 });
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,44 +1,46 @@
 import _ from 'lodash';
 
-const replacer = ' ';
-const spacesCount = 4;
-const getIndent = (depth) => replacer.repeat((depth * spacesCount) - 2);
-const getCloseBracket = (depth) => replacer.repeat((depth * spacesCount) - spacesCount);
+// const replacer = ' ';
+// const spacesCount = 4;
+const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(spacesCount * depth - 2);
+// const getCloseBracket = (depth) => replacer.repeat(depth  * spacesCount - spacesCount);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return `${data}`;
   }
   const entries = Object.entries(data);
-  const result = entries.map(([key, value]) => `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
-  const out = ['{', ...result, `${getCloseBracket(depth)}}`].join('\n');
+  const lines = entries.map(([key, value]) => `${getIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
+  //const out = ['{', ...lines, `${getIndent(depth)} }`].join('\n');
+  const out = [`{\n${lines.join('\n')}\n  ${getIndent(depth)}}`];
   return out;
 };
 
 const formatStylish = (tree) => {
-  const iter = (node, depth) => {
+  const iter = (node, depth = 1) => {
     const result = node.map((data) => {
       switch (data.type) {
         case 'unchanged':
-          return `${getIndent(depth)}   ${data.key}: ${stringify(data.value, depth + 1)}`;
+          return `${getIndent(depth)}  ${data.key}: ${stringify(data.value, depth)}`;
         case 'changed':
           return [
-            `${getIndent(depth)} - ${data.key}: ${stringify(data.value1, depth + 1)}\n${getIndent(depth)} + ${data.key}: ${stringify(data.value2, depth + 1)}`,
+            `${getIndent(depth)}- ${data.key}: ${stringify(data.value1, depth)}\n${getIndent(depth)}+ ${data.key}: ${stringify(data.value2, depth)}`,
           ];
         case 'added':
-          return `${getIndent(depth)} + ${data.key}: ${stringify(data.value, depth + 1)}`;
+          return `${getIndent(depth)}+ ${data.key}: ${stringify(data.value, depth)}`;
         case 'deleted':
-          return `${getIndent(depth)} - ${data.key}: ${stringify(data.value, depth + 1)}`;
+          return `${getIndent(depth)}- ${data.key}: ${stringify(data.value, depth)}`;
         case 'nested':
-          return `${getIndent(depth)}   ${data.key}: ${iter(data.children, depth + 1)}`;
+          return `${getIndent(depth)}  ${data.key}: {\n${iter(data.children, depth + 1)}\n${getIndent(depth)}  }`;
         default:
           throw new Error(`Uknown type ${data.type}`);
       }
     });
-    return ['{', ...result, `${getCloseBracket(depth)}}`].join('\n');
+     //return ['{', ...result, `${getIndent(depth)} }`].join('\n');
+    return result.join('\n');
   };
 
-  return iter(tree, 1);
+  return `{\n${iter(tree)}\n}`;
 };
 
 export default formatStylish;
