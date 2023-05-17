2410d29b808f80b08762710440a5a3c820fcf9ac
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-05-17 15:35:26 +0300

    final tests

--- src/parsers.js
+++ src/parsers.js
@@ -2,8 +2,6 @@ import yaml from 'js-yaml';
 
 const parseData = (content, format) => {
   switch (format) {
-    case 'json':
-      return JSON.parse(content);
     case 'yaml':
     case 'yml':
       return yaml.load(content);
