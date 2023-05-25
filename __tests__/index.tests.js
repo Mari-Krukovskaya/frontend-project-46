3fb7f42e68bb9f8de798a3869dce28948bc30a6d
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-05-25 10:01:52 +0300

    correction tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -3,22 +3,22 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'node:path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
+import json from '../src/formatters/json.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylish = fs.readFileSync(getFixturePath('expectedStylish_result.txt'), 'utf-8');
-const expectedPlain = fs.readFileSync(getFixturePath('expectedPlain_result.txt'), 'utf-8');
-const expectedJson = fs.readFileSync(getFixturePath('expectedJson_result.txt'), 'utf-8');
+const expectedStylish = readFile('expectedStylish_result.txt');
+const expectedPlain = readFile('expectedPlain_result.txt');
+const expectedJson = readFile('expectedJson_result.txt');
 
-const tests = ['json', 'yaml', 'yml'];
+const tests = ['json', 'yaml'];
 
 describe('Gendiff', () => {
-  test.each([
-    tests,
-  ])('Should be work with %s format', (format) => {
+  test.each(tests)('Should be work with %s format', (format) => {
     const filepath1 = getFixturePath(`file1.${format}`);
     const filepath2 = getFixturePath(`file2.${format}`);
     expect(genDiff(filepath1, filepath2)).toEqual(expectedStylish);
--- src/parsers.js
+++ src/parsers.js
@@ -3,12 +3,10 @@ import yaml from 'js-yaml';
 const parseData = (content, format) => {
   switch (format) {
     case 'yaml':
-    case 'yml':
       return yaml.load(content);
     case 'json':
     default:
       return JSON.parse(content);
   }
 };
-
 export default parseData;
